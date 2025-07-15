8eb4cc0262f849c53fcb04c35c1c291c9f13ac5b
Bkiser2000 <155510865+Bkiser2000@users.noreply.github.com>
2025-07-15 05:57:55 -0600

    Add files via upload

---
+++ perlexam.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+print "Hello, World\n";
+
+$employee_name = "Kiser";
+$employee_age = 23;
+$employee_salary = 440.5;
+
+print "Age = $employee_age\n";
+print "Name = $employee_name\n";
+print "Salary = $employee_salary\n";
